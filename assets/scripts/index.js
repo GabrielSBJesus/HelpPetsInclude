5fb04cd87cb8ae3040e5641c21005fd284a006b1
Gabriel <gabrielsantosbomjesus@gmail.com>
2022-08-30 16:07:26 -0300

    class add

---
+++ assets/scripts/index.js
@@ -0,0 +1,32 @@
+const user = document.querySelector('#user');
+const password = document.querySelector('#password');
+const btn = document.querySelector('#login');
+
+class Dados {
+    constructor() {
+        this.count = 0
+        this.inform = []
+    }
+
+    get click() {
+        return this.logar()
+    }
+
+    logar() {
+            if(user.value != '' && password.value != ''){
+                this.inform.push({
+                    id: this.count,
+                    login: user.value,
+                    password: password.value
+                    })
+                console.log(this.inform)
+            } else{
+                console.log('Dados errados')
+            }
+
+            this.count++
+    }
+}
+
+const d1 = new Dados()
+
